9cfcf2dfab4176c754e48cee7e43e9459e49b2fd
nejcc <nejc.cotic@gmail.com>
2024-12-31 15:05:56 +0100

    add navbar init

---
+++ app/Models/Navbar.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\NavbarFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Navbar extends Model
+{
+    /** @use HasFactory<NavbarFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'url',
+        'icon',
+        'parent_id',
+        'order',
+        'is_active',
+    ];
+
+    /**
+     * Relationship to get child items.
+     */
+    public function children(): HasMany
+    {
+        return $this->hasMany(Navbar::class, 'parent_id')->orderBy('order');
+    }
+
+    /**
+     * Relationship to get the parent item.
+     */
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(Navbar::class, 'parent_id');
+    }
+}
